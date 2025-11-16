37f0117a7c69c6be8637042464100b1cb785d30f
Therese Thindberg <Therethi@stud.ntnu.no>
2025-11-16 16:00:50 +0100

    added so heter could be tested

--- src/main/java/network/NodeClient.java
+++ src/main/java/network/NodeClient.java
@@ -1,5 +1,4 @@
 package network;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
@@ -8,10 +7,18 @@ import com.google.gson.JsonSerializer;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import entity.Actuator;
 import entity.Node;
+import entity.actuator.Actuator;
+
+import entity.actuator.Heater;
 import entity.sensor.Sensor;
 import entity.sensor.TemperatureSensor;
+import entity.actuator.Humidifier;
+import entity.actuator.DeHumidifier; 
+import entity.actuator.Ventilation;
+import entity.actuator.Heater;
+import entity.actuator.AirCondition;
+
 
 import java.io.*;
 import java.net.Socket;
@@ -57,7 +64,8 @@ public class NodeClient {
       try {
         while (true) {
           Thread.sleep(10000); // Update every 10 seconds
-          node.updateAllSensors();
+          node.applyActuatorEffects(); // actuator changes first
+          node.updateAllSensors();     // then natural sensor noise
           sendCurrentNode();
         }
       } catch (InterruptedException e) {
@@ -199,7 +207,7 @@ public class NodeClient {
       java.util.List<Sensor> sensors = new ArrayList<>();
       sensors.add(initSensor);
 
-      Actuator initActuator = new Actuator("1", "FAN");
+      Actuator initActuator = new Heater("1");
       java.util.List<Actuator> actuators = new ArrayList<>();
       actuators.add(initActuator);
 
