d856954238c519f33dcd1e283d053c421dff819c
Sebastian <sebastian.lid.nesvik@gmail.com>
2025-11-19 15:13:49 +0100

    TempTimeStamp

--- src/test/java/entity/sensor/TemperatureSensorTest.java
+++ src/test/java/entity/sensor/TemperatureSensorTest.java
@@ -1,7 +1,10 @@
 package entity.sensor;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -37,5 +40,17 @@ public class TemperatureSensorTest {
         assertEquals(20.0, sensor.getValue());
         assertNotNull(sensor.getTimestamp());
     }
+
+    @Test
+  public void updateValueWithParameter_setsValueAndUpdatesTimestamp() throws InterruptedException {
+    TemperatureSensor ts = new TemperatureSensor("t2", 0.0, 100.0);
+    LocalDateTime before = ts.getTimestamp();
+
+    Thread.sleep(5);
+
+    ts.updateValue(25.5);
+    assertEquals(25.5, ts.getValue(), 1e-6);
+    assertTrue(ts.getTimestamp().isAfter(before), "Timestamp should be updated after updateValue(double)");
+  }
     
 }
